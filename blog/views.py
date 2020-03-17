d73b548ff24e453cfbcf6b45f305eaafab5541d8
Michael Garcia <thecrazygm@gmail.com>
2020-03-17 05:47:52 -0500

    clean up for showing the public

--- blog/views.py
+++ blog/views.py
@@ -59,6 +59,11 @@ def blog_posts(request, author):
     posts = account.get_blog(start_entry_id=0, limit=10)
     for x in posts:
         x['body'] = markdown.markdown(x['body'])
+        x['body'] = sanitizer.sanitize(x['body'])
+        x['body'] = re.sub("(<h1>|<h2>)", "<h3>", x['body'])
+        x['body'] = re.sub(r"<img\b(?=\s)(?=(?:[^>=]|='[^']*'|=\"[^\"]*\"|=[^'\"][^\s>]*)*?\ssrc=['\"]([^\"]*)['\"]?)(?:[^>=]|='[^']*'|=\"[^\"]*\"|=[^'\"\s]*)*\"\s?\/?>",
+                           r'<img src=https://steemitimages.com/640x0/\1 >', x['body'])
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
