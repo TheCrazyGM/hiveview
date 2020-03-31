ef4001528b517e204d2b95b06742390c518532e2
Michael Garcia <thecrazygm@gmail.com>
2020-03-31 03:50:17 -0500

    markup fix

--- blog/views.py
+++ blog/views.py
@@ -2,46 +2,33 @@ import re
 
 import markdown
 import requests
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
+
 from bhive import Hive
 from bhive.account import Account
 from bhive.comment import Comment
 from bhive.discussions import Query
 from bhive.instance import set_shared_hive_instance
 from bhive.utils import construct_authorperm
-from django.shortcuts import get_object_or_404, redirect, render
-from django.utils import timezone
-from html_sanitizer import Sanitizer
 from hive import Hive as Hivepy
+from markupsafe import Markup
 
 #nodes = ["https://api.hive.blog", "https://anyx.io"]
 q = Query(limit=10)
-hvpy = Hivepy()#nodes=nodes)
-hv = Hive()#node=nodes)
+hvpy = Hivepy()  # nodes=nodes)
+hv = Hive()  # node=nodes)
 set_shared_hive_instance(hv)
-options = {
-    "tags": {
-        "a", "h1", "h2", "h3", "strong", "em", "p", "ul", "ol",
-        "li", "br", "sub", "sup", "hr", "img"
-    },
-    "attributes": {"a": ("href", "name", "target", "title", "id", "rel"), 'img': ('src', )},
-    "empty": {"hr", "a", "br", "img"},
-    "separate": {"a", "p", "li"},
-    "whitespace": {"br"},
-    "keep_typographic_whitespace": False,
-    "add_nofollow": False,
-    "autolink": True,
-    "is_mergeable": lambda e1, e2: True,
-}
-sanitizer = Sanitizer(settings=options)
 image_proxy = "https://images.hive.blog/480x0/"
 
 
 def strip(text):
-    text['body'] = markdown.markdown(text['body'], extensions=['nl2br', 'codehilite', 'pymdownx.extra','pymdownx.magiclink', 'pymdownx.betterem', 'pymdownx.inlinehilite'])
-    text['body'] = sanitizer.sanitize(text['body'])
-    #x['body'] = re.sub("(<h1>|<h2>)", "<h3>", x['body'])
+    text['body'] = markdown.markdown(text['body'], extensions=[
+                                     'nl2br', 'codehilite', 'pymdownx.extra', 'pymdownx.magiclink', 'pymdownx.betterem', 'pymdownx.inlinehilite'])
+    text['body'] = re.sub("(<h1>|<h2>)", "<h3>", text['body'])
     text['body'] = re.sub(r"<img\b(?=\s)(?=(?:[^>=]|='[^']*'|=\"[^\"]*\"|=[^'\"][^\s>]*)*?\ssrc=['\"]([^\"]*)['\"]?)(?:[^>=]|='[^']*'|=\"[^\"]*\"|=[^'\"\s]*)*\"\s?\/?>",
                           rf'<img src={image_proxy}\1 >', text['body'])
+    text['body'] = Markup(text['body'])
     return text
 
 
@@ -60,12 +47,14 @@ def hot(request):
             post = strip(post)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def latest(request):
     posts = hvpy.get_discussions_by_created(q)
     for post in posts:
         if post:
             post = strip(post)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def tag(request, tag):
     tag_q = Query(limit=10, tag=tag)
@@ -74,6 +63,7 @@ def tag(request, tag):
         if post:
             post = strip(post)
     return render(request, 'blog/post_list.html', {"posts": posts})
+
 
 def blog_posts(request, author):
     author = re.sub(r'(\/)', '', author)
@@ -107,17 +97,20 @@ def following(request, author):
     followers = account.get_following(raw_name_list=True, limit=100)
     return render(request, 'blog/follower.html', {'followers': followers})
 
-def request_author(request, *args, **kwargs):
-  if(request.GET.get('req_author')):
-    author =str(request.GET.get('author'))
-  else:
-    if 'author' in kwargs:
-        author = kwargs['author']
-  account = Account(author)
-  return render(request, 'blog/account.html', {'account': account})
 
-def market(request):    
-    return render(request, 'blog/market.html')    
+def request_author(request, *args, **kwargs):
+    if(request.GET.get('req_author')):
+        author = str(request.GET.get('author'))
+    else:
+        if 'author' in kwargs:
+            author = kwargs['author']
+    account = Account(author)
+    return render(request, 'blog/account.html', {'account': account})
+
+
+def market(request):
+    return render(request, 'blog/market.html')
+
 
 def post_new(request):
     """    if request.method == "POST":
